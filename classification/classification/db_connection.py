daba8c53601a32a9e5e6dbe950ad6ee6b3ce16ce
Jeerawat Chuachan <jeerawatchu@gmail.com>
2025-06-07 14:26:38 +0700

    refactor: move connection class to module

--- classification/classification/db_connection.py
+++ classification/classification/db_connection.py
@@ -2,12 +2,20 @@ import sys
 
 from dotenv import load_dotenv
 import mariadb
+import pandas as pd
 import sqlalchemy
 from sqlalchemy import create_engine
 
-from .credentials import MARIADB_HOST, MARIADB_PASSWORD, MARIADB_PORT, MARIADB_USERNAME, MARIADB_DATABASE
+from .credentials import (
+    MARIADB_HOST,
+    MARIADB_PASSWORD,
+    MARIADB_PORT,
+    MARIADB_USERNAME,
+    MARIADB_DATABASE,
+)
 
 load_dotenv()
+
 
 def get_mariadb_connection() -> mariadb.Connection:
     """Creates a connection using MariaDB Python connector.
@@ -35,6 +43,46 @@ def get_sqlalchemy_connection() -> sqlalchemy.Connection:
     Returns:
         sqlalchemy.Connection: SQLAlchemy connection.
     """
-    engine = create_engine(f"mariadb+mariadbconnector://{MARIADB_USERNAME}:{MARIADB_PASSWORD}@{MARIADB_HOST}:{MARIADB_PORT}/{MARIADB_DATABASE}")
+    engine = create_engine(
+        f"mariadb+mariadbconnector://{MARIADB_USERNAME}:{MARIADB_PASSWORD}@{MARIADB_HOST}:{MARIADB_PORT}/{MARIADB_DATABASE}"
+    )
     conn = engine.connect()
     return conn
+
+
+class FinancialDBConnection:
+    _accepted_connectors = ["mariadb", "sqlalchemy"]
+
+    def __init__(self):
+        self.conn_fn = None
+        self.conn = None
+
+    def _check_connection_exists(self):
+        if self.conn_fn is None or self.conn is None:
+            raise Exception("No connection found. Use `set_connection` first.")
+
+    def set_connection(self, connector: str):
+        if connector not in self._accepted_connectors:
+            raise ValueError(
+                f"Invalid connector. Accepted connectors are {self._accepted_connectors}"
+            )
+
+        if connector == "mariadb":
+            conn_fn = get_mariadb_connection
+        elif connector == "sqlalchemy":
+            conn_fn = get_sqlalchemy_connection
+
+        self.conn_fn = conn_fn
+        self.conn = conn_fn()
+
+    def query(self, sql: str) -> pd.DataFrame:
+        self._check_connection_exists()
+        try:
+            result = pd.read_sql(sql, con=self.conn)
+        except Exception as e:
+            print(
+                f"Query failed with exception: `{e}`. \nThe connection will be reinstantiated."
+            )
+            self.conn = self.conn_fn() # type: ignore
+            result = pd.read_sql(sql, con=self.conn)
+        return result
